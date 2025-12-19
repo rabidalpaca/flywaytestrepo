db714f3969ad222f96799c6a0ba9ba4abdeb088b
Roger <RogerYoung02@gmail.com>
2025-12-18 18:20:42 -0600

    More changes before I create another feature

--- dbo-sql/V2025.12.18.1__UserTable.sql
+++ dbo-sql/V2025.12.18.1__UserTable.sql
@@ -1,2 +1,11 @@
-Create Table dbo.test
-(id int)
+IF
+    NOT EXISTS (
+        SELECT * FROM sys.objects
+        WHERE object_id = OBJECT_ID(N'dbo.test') AND type in (N'U')
+    )
+    BEGIN
+        CREATE TABLE dbo.test
+        (
+            id INT
+        );
+    END
